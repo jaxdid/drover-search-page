771d98afaa29965c2a3170a35ec612496bdf38d0
Alain Lemaire <jaxdid@gmail.com>
2018-05-24 15:27:51 +0100

    Moves query/filter defaults out of main component, fixes default filter values

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,65 +2,18 @@ import React, { Component } from 'react'
 import SearchFiltersList from './SearchFiltersList'
 import SearchResultsList from './SearchResultsList'
 import fetchSearchResults from '../../lib/api'
+import {
+  getDefaultQuery,
+  getBaseFilterSettings
+} from '../../lib/utils'
 
 class Search extends Component {
   constructor () {
     super()
 
     this.state = {
-      query: {
-        'location': 'London, UK',
-        'max_distance': 50,
-        'order_by': 'price',
-        'order_direction': 'asc',
-        'page': 1,
-        'per_page': 15,
-        'price_max': 2500,
-        'price_min': 100,
-        'subscription_start_days': 30,
-        'vehicle_type': 'Consumer'
-      },
-      filters: {
-        location: {
-          label: 'Location',
-          defaultValue: 'London, UK'
-        },
-        startDate: {
-          label: 'Subscription starts within the next',
-          options: [2, 14, 30],
-          defaultValue: 30
-        },
-        maxDistance: {
-          label: 'Distance (radius in miles)',
-          options: [25, 50, 75, 100, 150, 200],
-          defaultValue: 50
-        },
-        vehicleMake: {
-          label: 'Vehicle Make',
-          options: {},
-          defaultValue: ''
-        },
-        transmission: {
-          label: 'Gearbox',
-          options: {},
-          defaultValue: ''
-        },
-        year: {
-          label: 'Year',
-          options: {},
-          defaultValue: ''
-        },
-        fuel: {
-          label: 'Fuel Type',
-          options: {},
-          defaultValue: ''
-        },
-        tags: {
-          label: 'Car Type',
-          options: {},
-          defaultValue: ''
-        }
-      },
+      query: getDefaultQuery(),
+      filters: getBaseFilterSettings(),
       results: []
     }
 
@@ -73,64 +26,25 @@ class Search extends Component {
 
   runSearch () {
     fetchSearchResults(this.state.query)
-      .then(({ metadata, data }) => {
-        const filters = {
-          location: {
-            label: 'Location',
-            defaultValue: 'London, UK',
-            staticValues: true
-          },
-          subscription_start_days: {
-            label: 'Subscription starts within the next',
-            options: [
-              {label: '2 Days', value: 2},
-              {label: '14 Days', value: 14},
-              {label: '30 Days', value: 30}
-            ],
-            staticValues: true
-          },
-          max_distance: {
-            label: 'Distance (radius in miles)',
-            options: [
-              {label: '25', value: 25},
-              {label: '50', value: 50},
-              {label: '75', value: 75},
-              {label: '100', value: 100},
-              {label: '150', value: 150},
-              {label: '200', value: 200}
-            ],
-            staticValues: true
-          },
-          vehicle_make: {
-            label: 'Vehicle Make',
-            options: metadata.aggregations.vehicle_make,
-            staticValues: false
-          },
-          transmission: {
-            label: 'Gearbox',
-            options: metadata.aggregations.transmission,
-            staticValues: false
-          },
-          year: {
-            label: 'Year',
-            options: metadata.aggregations.year,
-            staticValues: false
-          },
-          fuel: {
-            label: 'Fuel Type',
-            options: metadata.aggregations.fuel,
-            staticValues: false
-          },
-          tags: {
-            label: 'Car Type',
-            options: metadata.aggregations.tags,
-            staticValues: false
-          }
-        }
+      .then(({ metadata, data: results }) => {
+        const {
+          vehicle_make: vehicleMake,
+          transmission,
+          year,
+          fuel,
+          tags
+        } = metadata.aggregations
+        const filters = getBaseFilterSettings()
+
+        filters.vehicle_make.options = vehicleMake
+        filters.transmission.options = transmission
+        filters.year.options = year
+        filters.fuel.options = fuel
+        filters.tags.options = tags
 
         this.setState({
           filters,
-          results: data
+          results
         })
       })
   }
