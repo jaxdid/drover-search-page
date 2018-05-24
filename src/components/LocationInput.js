8d8405aaa0525b3b3d2a0a8689e964cdaacb4580
Alain Lemaire <jaxdid@gmail.com>
2018-05-24 15:53:52 +0100

    Wires up location filter to update search results

--- src/components/LocationInput.js
+++ src/components/LocationInput.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-function LocationInput () {
-  return <input className="filter-location" type="text" />
+class LocationInput extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      value: this.props.filterData.defaultValue
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange (event) {
+    const { value } = event.target
+    const { filterName, updateResults } = this.props
+
+    this.setState({value}, () => {
+      updateResults(filterName, value)
+    })
+  }
+
+  render () {
+    return (
+      <input
+        className="filter-location"
+        type="text"
+        value={this.state.value}
+        onChange={this.handleChange}
+      />
+    )
+  }
 }
 
 export default LocationInput
